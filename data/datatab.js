333c08f15368137b65fa3dfff3e9afa7f07fa0a3
Amadu <ndongamadou@gmail.com>
2017-06-27 13:44:20 +0000

    data tables fix

---
+++ data/datatab.js
@@ -0,0 +1,101 @@
+var datatab = [
+    {
+        "cluster": "CCCM",
+        "Adamawa": 1,
+        "Diffa": 0,
+        "Borno": 1,
+        "Yobe": 0,
+        "Lac": 7,
+        "Far-North": 12
+  },
+    {
+        "cluster": "Food Security",
+        "Adamawa": 12,
+        "Diffa": 23,
+        "Borno": 27,
+        "Yobe": 6,
+        "Lac": 18,
+        "Far-North": 19
+  },
+    {
+        "cluster": "Coordination",
+        "Adamawa": 1,
+        "Diffa": 1,
+        "Borno": 1,
+        "Yobe": 1,
+        "Lac": 3,
+        "Far-North": 1
+  },
+    {
+        "cluster": "Education",
+        "Adamawa": 4,
+        "Diffa": 13,
+        "Borno": 5,
+        "Yobe": 3,
+        "Lac": 7,
+        "Far-North": 0
+  },
+    {
+        "cluster": "Nutrition",
+        "Adamawa": 3,
+        "Diffa": 9,
+        "Borno": 8,
+        "Yobe": 1,
+        "Lac": 16,
+        "Far-North": 16
+  },
+    {
+        "cluster": "Protection",
+        "Adamawa": 8,
+        "Diffa": 14,
+        "Borno": 20,
+        "Yobe": 4,
+        "Lac": 14,
+        "Far-North": 16
+  },
+    {
+        "cluster": "WASH",
+        "Adamawa": 0,
+        "Diffa": 24,
+        "Borno": 19,
+        "Yobe": 0,
+        "Lac": 10,
+        "Far-North": 14
+  },
+    {
+        "cluster": "Shelter_NFI",
+        "Adamawa": 2,
+        "Diffa": 13,
+        "Borno": 5,
+        "Yobe": 2,
+        "Lac": 0,
+        "Far-North": 11
+  },
+    {
+        "cluster": "MultiSector",
+        "Adamawa": 0,
+        "Diffa": 10,
+        "Borno": 0,
+        "Yobe": 0,
+        "Lac": 0,
+        "Far-North": 0
+  },
+    {
+        "cluster": "Health",
+        "Adamawa": 0,
+        "Diffa": 18,
+        "Borno": 14,
+        "Yobe": 1,
+        "Lac": 0,
+        "Far-North": 16
+  },
+    {
+        "cluster": "Early Recoverly",
+        "Adamawa": 0,
+        "Diffa": 2,
+        "Borno": 0,
+        "Yobe": 0,
+        "Lac": 0,
+        "Far-North": 11
+  }
+];
